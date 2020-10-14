74005bf6caa30acaca6ec5b6ab3e50a347cad435
Kevin Gilmore <kevmisterio@gmail.com>
2020-10-14 13:47:40 -0400

    websocket support

--- http_server.js
+++ http_server.js
@@ -2,11 +2,27 @@
 
 const run  = async () => {
   const
+    http = require('http'),
     express = require('express'),
-    app = express()
+    ws = require('ws'),
+    app = express(),
+    server = http.createServer(app),
+    wss = new ws.Server({ server })
 
   app.use(express.static('www'))
-  app.listen(1974)
+
+  wss.on('connection', async ws => {
+
+    ws.on('message', async message => {})
+
+    const
+      heartbeat = setInterval(() => connection.send('ping'), 30000)
+
+    ws.on('close', () => clearInterval(heartbeat))
+    ws.on('error', () => clearInterval(heartbeat))
+  })
+
+  server.listen(1974, () => console.info(`Server listening on port ${1974}`))
 }
 
 run()
